9b3416d549f220cc8fbb8854e54d9f1c89518ccf
aril aril <akudewo14@gmail.com>
2021-06-12 16:35:42 +0700

    second commit

--- app/Imports/PasiensImport.php
+++ app/Imports/PasiensImport.php
@@ -2,20 +2,21 @@
 
 namespace App\Imports;
 
-use App\Pasien;
+use App\Models\Pasien;
 use Maatwebsite\Excel\Concerns\ToModel;
 
 class PasiensImport implements ToModel
 {
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
         return new Pasien([
-            //
+            'nama' => $row[1],
+            'alamat' => $row[2],
         ]);
     }
 }
